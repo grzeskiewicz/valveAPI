4c5b78bf66238cc3d2fd14b1c0240d904256df8b
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-01-30 13:57:03 +0100

    VALVE API

---
+++ valveAPI.js
@@ -0,0 +1,30 @@
+const Gpio=require('orange-pi-gpio');
+const firstValve=new Gpio({pin:22,mode:'out'});
+const secondValve=new Gpio({pin:23,mode:'out'});
+const thirdValve=new Gpio({pin:24,mode:'out'});
+const fourthValve=new Gpio({pin:25,mode:'out'});
+
+function motorON(channel){
+console.log('ON',channel)
+channel.write(0);
+}
+
+function motorOFF(channel){
+console.log('OFF',channel)
+channel.write(1);
+}
+
+console.log("STARTING ALL PUMPS: ")
+
+motorON(firstValve);
+setTimeout(()=>motorOFF(firstValve),20);
+
+motorON(secondValve);
+setTimeout(()=>motorOFF(secondValve),15);
+
+motorON(thirdValve);
+setTimeout(()=>motorOFF(thirdValve),10);
+
+motorON(fourthValve);
+setTimeout(()=>motorOFF(fourthValve),5);
+
