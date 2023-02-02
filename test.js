c61c711cdf0de0d7995b1817eca7823f372bb897
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-02-03 00:29:20 +0100

    Docker

--- test.js
+++ test.js
@@ -1,16 +1,41 @@
 var scheduler = require('nodejs-persistable-scheduler');
+var q = require('q');
+var methodArr = [];
+/***************************
+ ***************************
+  DONT CHANGE THE ORDER of mehtodArr!!!!
+  JUST PUSH THE NEW ADDED ONES AT THE TAIL!!!
+  **************************
+  **************************/
+method1 = function(param) {
+  var defer = Q.defer();
+  //functional codes for method1...
+  console.log("JESTEM TU")
+  return defer.promise;
+};
+
+methodArr.push({name:'METHOD1',method1});
+
+method2 = function(param) {
+  var defer = Q.defer();
+  //functional codes for method2...
+  return defer.promise;
+};
+methodArr.push({name:'METHOD2',method2});
 
 initialTask = function() {
+  var defer = Q.defer();
   var AM = new Date();
-  AM.setHours(0,14,0);
+  AM.setHours(0,33,0);
   scheduler.registerTask("daily",AM,'METHOD1',null,null).then(function(ID) {
     if (ID)
     {
-console.log("KURWA",ID)   
- }
+      defer.resolve(true);
+    }
   },function(err) {
-console.log(err);
+    defer.reject(err);
   });
+  return defer.promise;
 };
 
 var schedulerConfig = {
