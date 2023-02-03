9fd5935aaa1e0bcc164f920d7825dc9ab618b4bf
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-02-03 17:12:22 +0100

    Docker

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-const Agenda = require("@hokify/agenda");
+const Agenda = require("agenda");
 const agenda = new Agenda({ db: { address: "mongodb://51.83.131.118:27020/valveSchedule" } });
 
 agenda.define("send monthly billing report", async (job) => {
-  // some code that aggregates the database and send monthly billing report.
-  console.log("CHUJOOOOOOOOOOO")
 });
+
+console.log(await agenda.jobs());
+
 
 (async function () {
   // IIFE to give access to async/await
