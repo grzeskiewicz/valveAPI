fbd804b837ce7e77026c5c2a9ab39b49cf2fbd17
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-02-03 18:57:18 +0100

    Docker

--- test.js
+++ test.js
@@ -4,7 +4,9 @@ const agenda = new Agenda({ db: { address: "mongodb://51.83.131.118:27020/valveS
 agenda.define("cropID", async (job) => {
     console.log("CHUJKOOO")
 });
-   //agenda.start();
+agenda.on( "ready", function() {
+    agenda.start()
+});
   const job = agenda.create('cropID', {valve:1});
   job.repeatEvery("2 minutes", {timezone:'Europe/Warsaw',startDate:'2023-02-03T18:00:00+01:00',endDate:'2023-02-03T19:00:00+01:00'});
 job.save();
