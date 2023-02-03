288e924494a41292a0e9e14bcff8b3ab20d531d6
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-02-03 18:51:40 +0100

    Docker

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
 const Agenda = require("agenda");
 const agenda = new Agenda({ db: { address: "mongodb://51.83.131.118:27020/valveSchedule" } });
 
-agenda.define("send monthly billing report", async (job) => {
+agenda.define("cropID", async (job) => {
+    console.log("CHUJKOOO")
 });
-
-
-(async function () {
-  // IIFE to give access to async/await
-  await agenda.start();
-
-  await agenda.every("2 minutes", "send monthly billing report");
-})();
+   //agenda.start();
+  const job = agenda.create('cropID', {valve:1});
+  job.repeatEvery("2 minutes", {timezone:'Europe/Warsaw',startDate:'2023-02-03T18:00:00+01:00',endDate:'2023-02-03T19:00:00+01:00'});
+job.save();
+console.log('Job successfully saved');
