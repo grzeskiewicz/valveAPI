af9acc39a585352c3aee012b5d205f1d22e6ba83
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-02-04 23:23:31 +0100

    Docker

--- valveAPI.js
+++ valveAPI.js
@@ -190,7 +190,7 @@ async function scheduleWatering(req, res) {
     });
     await agenda.start();
     const days=req.body.start===req.body.stop ? start.date() : `${start.date()}-${stop.date()}`;
-    await job.repeatEvery(`32 22 ${days} ${stop.month()+1} *`, {
+    await job.repeatEvery(`${minute} 10 ${days} ${stop.month()+1} *`, {
       timezone: "Europe/Warsaw",
     });
     await job.save();
