8178a702d6259741f4c353ac9206d03d6e0f0834
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-02-04 16:11:14 +0100

    Docker

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 const Agenda = require("agenda");
 const agenda = new Agenda({ db: { address: "mongodb://51.83.131.118:27020/valveSchedule" } });
 
-agenda.define("hopla", async (job) => {
-    console.log("KURWA")
+agenda.define("hoplaxd", async (job) => {
+    console.log("KURWAMAĆ")
 });
 
 (async function () {
-    const job = agenda.create('hopla', {valve:1});
+    const job = agenda.create('hoplaxd', {valve:1});
 	await agenda.start();
 
 
-	await job.repeatEvery("12 16 4-10 2 *", {timezone:'Europe/Warsaw'});
+	await job.repeatEvery("13 16 4-10 2 *", {timezone:'Europe/Warsaw'});
     await job.save();
 })();
 
