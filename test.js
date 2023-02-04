0a2a285187d1de44c9a1c946ff6cd8b304f644b5
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2023-02-04 16:09:28 +0100

    Docker

--- test.js
+++ test.js
@@ -8,12 +8,9 @@ agenda.define("hopla", async (job) => {
 (async function () {
     const job = agenda.create('hopla', {valve:1});
 	await agenda.start();
-    const start=new Date();
-    start.setHours(14,25);
-    const end=new Date();
-    end.setHours(14,28);
 
-	await job.repeatEvery("2 minutes", {timezone:'Europe/Warsaw',startDate:start,endDate:end});
+
+	await job.repeatEvery("12 16 4-10 2 *", {timezone:'Europe/Warsaw'});
     await job.save();
 })();
 
